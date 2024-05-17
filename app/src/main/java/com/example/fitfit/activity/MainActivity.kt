fe2348387147451a2ff296f75c69ab1b4961d62b
sam <gjskatn123@naver.com>
2024-05-17 18:20:49 +0900

    feature : 바텀 네비게이션 뷰 구현

--- app/src/main/java/com/example/fitfit/activity/MainActivity.kt
+++ app/src/main/java/com/example/fitfit/activity/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.fitfit.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.fitfit.R
+import com.example.fitfit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        //디벨롭에 푸시
+        // 네비게이션을 담는 호스트
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        // 네비게이션 컨트롤러
+        val navController = navHostFragment.navController
+
+        // 바텀 네비게이션 뷰와 네비게이션을 묶는다.
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
+        binding.bottomNavigationView.itemIconTintList = null
+
     }
 }
