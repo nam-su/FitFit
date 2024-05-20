95aed8a9420ea7622d41212fe075dbb6af16bd64
sam <gjskatn123@naver.com>
2024-05-20 16:35:31 +0900

    feature : 스플래시 프래그먼트 실행되면서 로그인 프래그먼트로 전환

--- app/src/main/java/com/example/fitfit/activity/MainActivity.kt
+++ app/src/main/java/com/example/fitfit/activity/MainActivity.kt
@@ -2,19 +2,42 @@ package com.example.fitfit.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.fitfit.R
 import com.example.fitfit.databinding.ActivityMainBinding
+import com.example.fitfit.fragment.LoginFragment
+import com.example.fitfit.fragment.SplashFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val fragmentManager = supportFragmentManager
+    private var fragmentTransaction = fragmentManager.beginTransaction()
+    private val splashFragment = SplashFragment()
+    private val loginFragment = LoginFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setView()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startSplash()
+    }
+
+    fun setView() {
 
         // 네비게이션을 담는 호스트
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
@@ -29,4 +52,23 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.itemIconTintList = null
 
     }
+
+    // 스플래시
+    fun startSplash() {
+
+        CoroutineScope(Dispatchers.Main).launch {
+
+            fragmentTransaction.add(R.id.fragmentSplash,splashFragment)
+            fragmentTransaction.commit()
+
+            delay(3000)
+
+            fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentSplash,loginFragment)
+            fragmentTransaction.commit()
+
+        }
+
+    }
+
 }
