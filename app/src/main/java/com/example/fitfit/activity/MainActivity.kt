2d317e7464494ea00d72192ed7c128571343006b
sam <gjskatn123@naver.com>
2024-05-20 14:27:36 +0900

    feature : 메인 엑티비티 바텀 네비게이션

--- app/src/main/java/com/example/fitfit/activity/MainActivity.kt
+++ app/src/main/java/com/example/fitfit/activity/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //디벨롭에 푸시
     }
 }
