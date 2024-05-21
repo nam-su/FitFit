f64350bc41705a13953d939546c0273935c97033
sam <gjskatn123@naver.com>
2024-05-21 22:00:06 +0900

    Feature : 로그인 기능 구현

--- app/src/main/java/com/example/fitfit/activity/MainActivity.kt
+++ app/src/main/java/com/example/fitfit/activity/MainActivity.kt
@@ -2,104 +2,106 @@ package com.example.fitfit.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.fitfit.R
 import com.example.fitfit.databinding.ActivityMainBinding
+import com.example.fitfit.fragment.HomeFragment
 import com.example.fitfit.fragment.LoginFragment
 import com.example.fitfit.fragment.SplashFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "메인액티비티"
+
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var fragmentManager: FragmentManager
-    private lateinit var fragmentTransaction: FragmentTransaction
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
 
     private val splashFragment = SplashFragment()
     private val loginFragment = LoginFragment()
+    private val homeFragment = HomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setView()
         setVariable()
-
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(TAG, "onStart: ")
         startSplash()
     } // onStart()
 
 
-    // 뷰관련 초기화 메서드
+    // 뷰관련 초기화
     private fun setView() {
 
+        // 시작할때 상태바색 바꾸기.
+        window.statusBarColor = ContextCompat.getColor(this, R.color.personal)
+
         // 네비게이션을 담는 호스트
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
         // 네비게이션 컨트롤러
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         // 바텀 네비게이션 뷰와 네비게이션을 묶는다.
-        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
 
         // 기본 아이콘 색상 적용 안함
         binding.bottomNavigationView.itemIconTintList = null
 
     } // setView()
 
+
+    // 변수 초기화
     private fun setVariable() {
 
         binding.lifecycleOwner = this
-        fragmentManager = supportFragmentManager
-        fragmentTransaction = fragmentManager.beginTransaction()
 
-    }
+    } // setVariable()
 
 
-    // 스플래시 시작하는 메서드
+    // 스플래시 시작
     private fun startSplash() {
 
+        // 바텀네비게이션 view gone 해놓고 스플래시 프래그먼트로 진입
         binding.bottomNavigationView.visibility = View.GONE
 
         // UI 변화 및 3초 딜레이 주기 위해서 코루틴 사용
         CoroutineScope(Dispatchers.Main).launch {
 
-            fragmentTransaction.replace(R.id.linearLayoutMainLayout,splashFragment)
-            fragmentTransaction.commit()
-
             delay(3000)
 
-            // transaction 초기화 후 replace 로 다른 프래그먼트를 띄워준다.
-            fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.constraintLayoutSplash,loginFragment)
-            fragmentTransaction.commit()
+            navController.navigate(R.id.action_splashFragment_to_loginFragment)
 
         }
 
     } // startSplash()
 
 
-    // 로그인 성공했을 때에 대한 간단한 화면전환 메서드
-    fun successLogin() {
+    // 로그인 했을때 홈 프래그먼트로 전환.
+    fun changeNavHostFragment() {
 
-        fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.remove(loginFragment)
-        fragmentTransaction.remove(splashFragment)
-        fragmentTransaction.commit()
+        navController.navigate(R.id.action_loginFragment_to_homeFragment)
         binding.bottomNavigationView.visibility = View.VISIBLE
 
-    } // successLogin()
+    } // changeNavHostFragment()
 
 }
--- app/src/main/java/com/example/fitfit/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/fitfit/fragment/LoginFragment.kt
@@ -1,10 +1,12 @@
 package com.example.fitfit.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentTransaction
@@ -12,12 +14,18 @@ import com.example.fitfit.R
 import com.example.fitfit.activity.MainActivity
 import com.example.fitfit.databinding.FragmentLoginBinding
 import com.example.fitfit.viewModel.LoginViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginFragment() : Fragment() {
 
-    private lateinit var binding: FragmentLoginBinding
-    private lateinit var viewModel: LoginViewModel
+    val TAG = "로그인 프래그먼트"
 
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var loginViewModel: LoginViewModel
+
+    // onCreateView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
@@ -26,18 +34,23 @@ class LoginFragment() : Fragment() {
 
     } // onCreateView()
 
+
+    // onViewCreated
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setVariable()
         setClickListener()
+        setObserve()
 
     } // onViewCreated()
+
 
     // 변수 초기화
     private fun setVariable() {
 
-        viewModel = LoginViewModel()
+        loginViewModel = LoginViewModel()
+        binding.loginViewModel = loginViewModel
 
     } // setVariable
 
@@ -45,12 +58,26 @@ class LoginFragment() : Fragment() {
     // 클릭리스너 초기화
     private fun setClickListener() {
 
-        binding.buttonLogin.setOnClickListener {
+    } // setClickListener()
 
-            (activity as MainActivity).successLogin()
+
+    // Observe 관련 메서드
+    private fun setObserve() {
+
+        loginViewModel.isSuccessLogin.observe(viewLifecycleOwner) {
+
+            Log.d(TAG, "setClickListener: 버튼 눌렀을때 isSuccess" + loginViewModel.isSuccessLogin.value)
+
+            when(it) {
+
+                true -> CoroutineScope(Dispatchers.Main).launch {(activity as MainActivity).changeNavHostFragment()}
+
+                false -> Toast.makeText(activity,"아이디 혹은 비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
+
+            }
 
         }
 
-    } // setClickListener()
+    } // setObserve()
 
 }
