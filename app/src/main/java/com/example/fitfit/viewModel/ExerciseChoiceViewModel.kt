6461b686b658addd0e0ae98300bd52244056753e
sam <gjskatn123@naver.com>
2024-05-29 15:19:50 +0900

    Docs : 주석 추가

--- app/src/main/java/com/example/fitfit/viewModel/ExerciseChoiceViewModel.kt
+++ app/src/main/java/com/example/fitfit/viewModel/ExerciseChoiceViewModel.kt
@@ -9,10 +9,11 @@ class ExerciseChoiceViewModel: ViewModel(){
     private val exerciseChoiceModel = ExerciseChoiceModel()
 
 
+    // 모델의 exerciseChoiceList를 가져오는 메서드.
     fun setRecyclerViewExerciseChoice(): ArrayList<ExerciseChoice>{
 
         return exerciseChoiceModel.setExerciseChoiceList()
 
-    }
+    } // setRecyclerViewExerciseChoice()
 
 }
