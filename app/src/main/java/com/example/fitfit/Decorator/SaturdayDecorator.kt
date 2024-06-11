a26ade0f851a6d8029361bf865db464dcc8d2c6b
harry <gudtis9556@naver.com>
2024-06-11 17:05:39 +0900

    Feat : 캘린더뷰 커스텀

---
+++ app/src/main/java/com/example/fitfit/Decorator/SaturdayDecorator.kt
@@ -0,0 +1,21 @@
+package com.example.fitfit.Decorator
+
+import android.graphics.Color
+import android.text.style.ForegroundColorSpan
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import java.util.Calendar
+
+class SaturdayDecorator : DayViewDecorator {
+    private val calendar = Calendar.getInstance()
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        day.copyTo(calendar)
+        val weekDay = calendar[Calendar.DAY_OF_WEEK]
+        return weekDay == Calendar.SATURDAY
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(ForegroundColorSpan(Color.BLUE))
+    }
+}
---
+++ app/src/main/java/com/example/fitfit/Decorator/SundayDecorator.kt
@@ -0,0 +1,21 @@
+package com.example.fitfit.Decorator
+
+import android.graphics.Color
+import android.text.style.ForegroundColorSpan
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import java.util.Calendar
+
+class SundayDecorator : DayViewDecorator {
+    private val calendar = Calendar.getInstance()
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        day.copyTo(calendar)
+        val weekDay = calendar[Calendar.DAY_OF_WEEK]
+        return weekDay == Calendar.SUNDAY
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(ForegroundColorSpan(Color.RED))
+    }
+}
