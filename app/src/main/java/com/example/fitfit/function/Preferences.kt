efa04e97d93110c66262ba5af7faa680f6fc8c5a
sam <gjskatn123@naver.com>
2024-06-21 18:24:52 +0900

    Chore : 운동 날짜 비교해서 카운트 초기화 코드 시점 변경

--- app/src/main/java/com/example/fitfit/function/Preferences.kt
+++ app/src/main/java/com/example/fitfit/function/Preferences.kt
@@ -27,19 +27,19 @@ class Preferences(context: Context) {
     // 현재 제공하는 모든 운동에 관한 내용 더미 데이터
     init {
 
-        allExerciseList.add(PoseExercise(0,"스쿼트","기본 스쿼트",0,0,0))
-        allExerciseList.add(PoseExercise(0,"푸시업","기본 푸시업",0,0,0))
-        allExerciseList.add(PoseExercise(0,"런지","기본 런지",0,0,0))
+        allExerciseList.add(PoseExercise(0, "스쿼트", "기본 스쿼트", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "푸시업", "기본 푸시업", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "런지", "기본 런지", 0, 0, 0))
 
-        allExerciseList.add(PoseExercise(0,"스쿼트","와이드 스쿼트",0,0,0))
-        allExerciseList.add(PoseExercise(0,"푸시업","와이드 푸시업",0,0,0))
-        allExerciseList.add(PoseExercise(0,"런지","왼쪽 런지",0,0,0))
+        allExerciseList.add(PoseExercise(0, "스쿼트", "와이드 스쿼트", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "푸시업", "와이드 푸시업", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "런지", "왼쪽 런지", 0, 0, 0))
 
-        allExerciseList.add(PoseExercise(0,"스쿼트","내로우 스쿼트",0,0,0))
-        allExerciseList.add(PoseExercise(0,"푸시업","내로우 푸시업",0,0,0))
-        allExerciseList.add(PoseExercise(0,"런지","오른쪽 런지",0,0,0))
+        allExerciseList.add(PoseExercise(0, "스쿼트", "내로우 스쿼트", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "푸시업", "내로우 푸시업", 0, 0, 0))
+        allExerciseList.add(PoseExercise(0, "런지", "오른쪽 런지", 0, 0, 0))
 
-        for (i:Int in 3 until allExerciseList.size) {
+        for (i: Int in 3 until allExerciseList.size) {
 
             allExerciseList[i].isPrimium = 1
 
@@ -84,7 +84,6 @@ class Preferences(context: Context) {
     } // getUserNickname()
 
 
-
     // myExerciseList 불러오기
     fun getMyAllExerciseList(): ArrayList<PoseExercise> {
 
@@ -99,17 +98,19 @@ class Preferences(context: Context) {
 
     // myExerciseList 저장
     private fun setMyAllExerciseList(exerciseList: ArrayList<PoseExercise>) {
-            Log.d(TAG, "setMyExerciseList 호출")
+        Log.d(TAG, "setMyExerciseList 호출")
 
         myAllExerciseList.clear()
         myAllExerciseList.addAll(exerciseList)
 
         myAllExerciseList.forEach {
-            Log.d(TAG, "[set : ${it.category},${it.exerciseName},${it.exerciseCount},${it.goalExerciseCount},${it.date},${it.checkList}]: ")
-            }
+            Log.d(
+                TAG,
+                "[set : ${it.category},${it.exerciseName},${it.exerciseCount},${it.goalExerciseCount},${it.date},${it.checkList}]: "
+            )
+        }
 
     }
-
 
 
     // 서버에서 유저 운동 정보 불러온 후 리스트에 저장하는 메서드
@@ -117,57 +118,29 @@ class Preferences(context: Context) {
 
         setMyAllExerciseList(exerciseList)
 
-        CoroutineScope(Dispatchers.Main).launch{
+        CoroutineScope(Dispatchers.Main).launch {
 
-        // 시간 복잡도를 O(n * m) 에서 O(n + m) 으로 줄이기 위한 맵 사용
-        // 각 운동의 이름을 key 값으로 해서 맵을 만들고 for문 사용
-        val exerciseMap = allExerciseList.associateBy { it.exerciseName }.toMutableMap()
+            // 시간 복잡도를 O(n * m) 에서 O(n + m) 으로 줄이기 위한 맵 사용
+            // 각 운동의 이름을 key 값으로 해서 맵을 만들고 for문 사용
+            val exerciseMap = allExerciseList.associateBy { it.exerciseName }.toMutableMap()
 
-        for (exercise in exerciseList) {
+            for (exercise in exerciseList) {
 
-            if (exercise.checkList == 1) {
+                if (exercise.checkList == 1) {
 
-                exerciseMap[exercise.exerciseName] = exercise
+                    exerciseMap[exercise.exerciseName] = exercise
+
+                }
+
+                val myExerciseList = exerciseMap.values.filter { it.checkList == 1 }
+
+                setMyPoseExerciseList(ArrayList(myExerciseList))
 
             }
 
-            val myExerciseList = exerciseMap.values.filter { it.checkList == 1 }
-
-//            setMyPoseExerciseList(compareExerciseDate(ArrayList(myExerciseList)))
-
         }
-
-       }
-
-
 
     } // setUserExerciseInfoList()
-
-
-    // 최근 운동한 날짜가 오늘과 다르다면 운동 카운트 초기화 하는 메서드
-    private fun compareExerciseDate(myExerciseList: ArrayList<PoseExercise>): ArrayList<PoseExercise> {
-        // 오늘 날짜
-        val todayDate = LocalDate.now()
-
-        // 내 운동리스트 순회
-        for (exercise in myExerciseList) {
-
-            // 최근 운동의 운동한 날짜.
-            val exerciseDate = LocalDate.ofEpochDay(exercise.date / (24 * 60 * 60 * 1000))
-
-            // 오늘날짜와 마지막 운동 기록의 날짜가 일치하지 않을 때
-            if(todayDate != exerciseDate) {
-
-                exercise.date = System.currentTimeMillis()
-                exercise.exerciseCount = 0
-
-            }
-
-        }
-
-        return myExerciseList
-
-    } // compareExerciseDate()
 
 
     // 유저 정보 쉐어드에 저장 하는 메서드
@@ -182,15 +155,14 @@ class Preferences(context: Context) {
     } // setUser()
 
 
-
     // 스케쥴링한 운동 리스트 불러오는 메서드
     fun getMyPoseExerciseList(): ArrayList<PoseExercise> {
 
-        val sharedPoseExerciseList = preferences.getString("poseExerciseList","")
+        val sharedPoseExerciseList = preferences.getString("poseExerciseList", "")
 
         val poseExerciseList = ArrayList<PoseExercise>()
 
-        if(!sharedPoseExerciseList.equals("")) {
+        if (!sharedPoseExerciseList.equals("")) {
 
             val jsonArray = JSONArray(sharedPoseExerciseList)
 
@@ -226,7 +198,7 @@ class Preferences(context: Context) {
 
         val jsonArray = Gson().toJson(poseExerciseList)
 
-        editor.putString("poseExerciseList",jsonArray)
+        editor.putString("poseExerciseList", jsonArray)
         editor.apply()
 
     } // setExerciseSchedule()
@@ -239,7 +211,7 @@ class Preferences(context: Context) {
 
         for (i in 0 until poseExerciseList.size) {
 
-            when(poseExerciseList[i].exerciseName){
+            when (poseExerciseList[i].exerciseName) {
 
                 // 이름이 같은 운동이 있으면 갱신해 준다
                 poseExercise.exerciseName -> poseExerciseList[i] = poseExercise
@@ -269,7 +241,7 @@ class Preferences(context: Context) {
 
         val sharedExerciseName = preferences.getString(exerciseName, "")
 
-        val jsonObject= JSONObject(sharedExerciseName!!)
+        val jsonObject = JSONObject(sharedExerciseName!!)
 
         return PoseExercise(
 
@@ -285,13 +257,11 @@ class Preferences(context: Context) {
     } // loadPoseExercise
 
 
-
     //로그아웃 또는 회원탈퇴 시 쉐어드 모든 데이터 삭제
     fun removeAll() {
         editor.clear()
         editor.apply()
     }
-
 
 
     // 제공되는 운동 리스트 리턴
