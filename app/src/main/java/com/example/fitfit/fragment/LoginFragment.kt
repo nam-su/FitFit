b1f37f1433746ce17c3a7df226e01c5210341b21
sam <gjskatn123@naver.com>
2024-06-30 18:17:09 +0900

    Feat : 카카오 로그인 구현

--- app/src/main/java/com/example/fitfit/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/fitfit/fragment/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.fitfit.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,14 @@ import com.example.fitfit.R
 import com.example.fitfit.activity.MainActivity
 import com.example.fitfit.databinding.FragmentLoginBinding
 import com.example.fitfit.viewModel.LoginViewModel
+import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.model.KakaoSdkError
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 
 class LoginFragment: Fragment() {
 
@@ -25,7 +34,7 @@ class LoginFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
-
+        KakaoSdk.init(requireContext(),"d997bc71e6bb7cad42042752aa3d4f9f")
         return binding.root
 
     } // onCreateView()
@@ -65,6 +74,13 @@ class LoginFragment: Fragment() {
             val bundle = bundleOf("startingPoint" to "loginFragment")
             this.findNavController().navigate(R.id.action_loginFragment_to_findPasswordFragment, bundle)
         }
+
+        binding.ImageButtonKakaoLogin.setOnClickListener {
+
+            setKaKaoLogin()
+
+        }
+
     }
 
 
@@ -91,6 +107,64 @@ class LoginFragment: Fragment() {
             }
 
         }
+    }
+
+    private fun setKaKaoLogin() {
+
+        // 카카오톡 설치 확인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+
+            // 카카오톡 로그인
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                // 로그인 실패 부분
+                if (error != null) {
+
+                    Log.e(TAG, "로그인 실패 $error")
+
+                    // 사용자가 취소
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
+
+                        return@loginWithKakaoTalk
+
+                    }
+                    // 다른 오류
+
+                    else {
+
+                        UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = emailLoginCallback) // 카카오 이메일 로그인
+
+                    }
+                }
+
+                // 로그인 성공 부분
+                else if (token != null) {
+
+                    Log.e(TAG, "로그인 성공 ${token.accessToken}")
+
+                }
+
+            }
+        }
+
+        else {
+
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = emailLoginCallback) // 카카오 이메일 로그인
+
+        }
+
+    } // setKaKaoLogin()
+
+    // 이메일 로그인 콜백
+    private val emailLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+
+        Log.d(TAG, "이메일 로그인 콜백: ")
+        
+        if (error != null) {
+            Log.e(TAG, "로그인 실패 $error")
+        } else if (token != null) {
+            Log.e(TAG, "로그인 성공 ${token.accessToken}")
+        }
+
     }
 
 }
